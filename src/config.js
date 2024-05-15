ea3efef2a9c8e869f71fbf7fbc82940dfdf0471f
oscar1711 <oscaredu1711@gmail.com>
2024-05-14 20:31:41 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports= {
     api: {
-        baseURL: "http://localhost:5000"
+        baseURL: "https://backend-proyecto-final-h4on.onrender.com"
+        //"http://localhost:5000"
     }
 }
